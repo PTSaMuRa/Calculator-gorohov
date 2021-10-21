13bb3f3360a10a2e3661abc9a0672137a03bb0e6
PTSaMuRa <91196218+PTSaMuRa@users.noreply.github.com>
2021-10-21 10:57:26 +0300

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,18 @@
+import math
+
+print('Введите число')
+a = float(input())
+print('Введите число')
+b = float(input())
+
+print(a + b)
+print(a - b)
+print(b - a)
+print(a * b)
+print(a / b)
+print(b / a)
+print(a ** b)
+print(b ** a)
+print(a math.sin)
+print(b math.sin)
+print('a', 'b')
