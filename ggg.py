9b923f6cd025c3e4b7d091e703650dd82fad322c
PTSaMuRa <91196218+PTSaMuRa@users.noreply.github.com>
2021-10-21 11:01:18 +0300

    Add files via upload

---
+++ ggg.py
@@ -0,0 +1,14 @@
+import math
+
+a = input('Enter your first number:')
+b = input('Enter your second number:')
+print('{} + {} = '.format(a, b))
+
+print(a + b)
+print(a - b)
+print(b - a)
+print(a * b)
+print(a / b)
+print(b / a)
+print(a ** b)
+print(b ** a)
